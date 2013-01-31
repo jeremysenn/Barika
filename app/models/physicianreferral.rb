d6c026e4bd848b0588eec5fda9d76e504bd743b4
Jeremy Senn <jeremysenn@Jeremy-Senns-MacBook-Pro.local>
2013-01-30 17:32:08 -0800

    Added physicianreferral model

---
+++ app/models/physicianreferral.rb
@@ -0,0 +1,9 @@
+class Physicianreferral < ActiveRecord::Base
+  # new columns need to be added here to be writable through mass assignment
+#  attr_accessible :user_id
+
+  set_table_name 'Physicianreferral'
+#  set_primary_key 'clientid'
+
+
+end
